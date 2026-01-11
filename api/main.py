cd7c85fdb2a7fbde6b020526334802a4c0e0d902
Morgane Geoffroy <morgane.geoffroy34@gmail.com>
2026-01-11 12:43:31 +0100

    ci/cd

--- api/main.py
+++ api/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from api.routers import healthcheck, static, dynamic, live, merged
+from prometheus_fastapi_instrumentator import Instrumentator # <--- Import
 
 app = FastAPI(
 	title = "DST Airlines API",
 	description = "API REST pour suivi des vols en temps réel",
 	version = "1.0.0"
 )
+
+Instrumentator().instrument(app).expose(app)
 
 # Inclure tous les routers
 app.include_router(healthcheck.router)
